c6a7141a3db10cb021eceb79cb633841463d7e02
Guest User <Guest@Marcos-MacBook.local>
2014-06-01 19:07:33 +0100

    Implemented basic api to insert data into neo4j db.

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(4000, '0.0.0.0');
-console.log('Server running at http://0.0.0.0:4000/');
+var neo4j = require('node-neo4j');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser());
+
+var db = new neo4j('http://192.168.63.4:7474');
+
+app.get('/node/:id', function(req, res){
+	db.readNode(req.params['id'], function(err, node) {
+		if(err) res.send(500);
+    	res.send(node);
+    });
+});
+
+app.post('/node', function(req, res) {
+	db.insertNode(req.body, function(err, node) {
+    	if(err) res.send(500);
+    	res.send(200);
+	});
+});
+
+app.listen(4001);
