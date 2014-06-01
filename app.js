02793851f9822e264554b229a441dee73553d05d
Luca <luca.lanziani@gmail.com>
2014-06-01 18:27:53 +0100

    Move server on port 4000

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(3000, '0.0.0.0');
-console.log('Server running at http://0.0.0.0:3000/');
+}).listen(4000, '0.0.0.0');
+console.log('Server running at http://0.0.0.0:4000/');
